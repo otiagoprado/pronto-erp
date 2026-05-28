7e55844b4811c62e5a57ad77b7cace9e194b77b5
otiagoprado <otiagoprado@gmail.com>
2026-05-28 15:40:47 -0300

    Delete hooks.py

--- hooks.py
+++
@@ -1,9 +0,0 @@
-app_name = "pronto_erp"
-app_title = "ERP Pronto"
-app_publisher = "Tiago Prado"
-app_description = "Tema customizado e otimizações para o ERP Pronto"
-app_email = "otiagoprado@gmail.com"
-app_license = "mit"
-
-# Injeção Nativa de CSS na Desk (Área Administrativa)
-app_include_css = "/assets/pronto_erp/css/ios_theme.css"
