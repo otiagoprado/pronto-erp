52bb7e0c081c46056be33e3858c74e3a8455ec50
otiagoprado <otiagoprado@gmail.com>
2026-05-28 11:13:15 -0300

    Create hooks.py

---
+++ hooks.py
@@ -0,0 +1,10 @@
+app_name = "pronto-erp"
+app_title = "Pronto ERP"
+app_publisher = "Tiago Prado"
+app_description = "ERP com interface minimalista inspirada no iOS"
+app_icon = "octicon octicon-device-desktop"
+app_color = "grey"
+app_version = "0.0.1"
+
+# Aqui avisamos o Frappe para injetar o nosso arquivo CSS em todas as telas do sistema
+app_include_css = "/assets/pronto-erp/css/ios_theme.css"
