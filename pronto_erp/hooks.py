fbcb31ed10076df28791196049f1b808f4c86674
otiagoprado <otiagoprado@gmail.com>
2026-05-28 16:40:26 -0300

    Update hooks.py

--- pronto_erp/hooks.py
+++ pronto_erp/hooks.py
@@ -5,10 +5,5 @@ app_description = "Tema customizado e otimizações para o ERP Pronto"
 app_email = "otiagoprado@gmail.com"
 app_license = "mit"
 
-# A exigência de versão no formato exato que a Frappe Cloud pede
-required_apps = {
-    "frappe": ">=15.0.0 <16.0.0"
-}
-
 # Injeção Nativa de CSS na Desk (Área Administrativa)
 app_include_css = "/assets/pronto_erp/css/ios_theme.css"
